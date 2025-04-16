ee7c26bff6ef63756cb2aa3ddf91fbb68aba382b
KCTHE0 <163514707+KCTHE0@users.noreply.github.com>
2025-04-16 09:00:43 +0000

    test again

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const feedContainer = document.getElementById('rss-feed');
 const searchInput = document.getElementById('searchInput');
 
 const RSS_URL = 'https://api.rss2json.com/v1/api.json?rss_url=http://www.usine-digitale.fr/rss';
-const image = item.enclosure?.link || 'https://via.placeholder.com/600x300?text=Pas+de+visuel';
-
 
 let allArticles = [];
 
@@ -17,9 +15,12 @@ fetch(RSS_URL)
 function renderArticles(articles) {
   feedContainer.innerHTML = '';
   articles.forEach(item => {
+    const image = item.enclosure?.link || 'https://via.placeholder.com/600x300?text=Pas+de+visuel';
+    
     const article = document.createElement('div');
     article.classList.add('article');
     article.innerHTML = `
+      <img src="${image}" alt="Illustration article">
       <h3><a href="${item.link}" target="_blank">${item.title}</a></h3>
       <p>${new Date(item.pubDate).toLocaleDateString('fr-FR')}</p>
       <p>${item.description.substring(0, 150)}...</p>
