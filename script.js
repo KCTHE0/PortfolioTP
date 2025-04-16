8b85293b9dffb7c3f575c6d4fc69acc14d20818a
KCTHE0 <163514707+KCTHE0@users.noreply.github.com>
2025-04-16 09:49:48 +0000

    rss

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const feedContainer = document.getElementById('rss-feed');
 
-const RSS_URL = 'https://api.rss2json.com/v1/api.json?rss_url=https://www.gamekult.com/feed.xml';
+const RSS_URL = 'https://www.usine-digitale.fr/rss';
 
 fetch(RSS_URL)
   .then(res => res.json())
